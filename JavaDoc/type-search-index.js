a2a995f45c5e8daf01d1d64a64fac6772650da4f
Dinuka Gayashan <dinukagayashankasthuriarachchi@gmail.com>
2022-02-09 17:55:47 +0530

    Delete JavaDoc directory

--- JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Bank"},{"p":"<Unnamed>","l":"Bank_Account"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
